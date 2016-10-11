2115184d5f3c66d8096f9d5bc3e6c266eb521fa0
seng299Project <francesyu90@yahoo.com>
2016-10-10 21:55:01 -0700

    Implement form validation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+function validateCCNum(ccNum) {
+    var isNum = /^\d+$/.test(ccNum);
+    if(!isNum || isNum.length < 16 || isNum.length > 16) {
+        return false;
+    }
+    var numArr = ccNum.split("");
+    var numArrLess1E = [];
+    for(var i = 0; i < numArr.length - 1; i++) {
+        numArrLess1E[numArr.length - i - 2] = parseInt(numArr[i]);
+    }
+    var sum = 0;
+    for(var i = 0; i < numArrLess1E.length; i++) {
+        if(i % 2 == 0) {
+            numArrLess1E[i] = numArrLess1E[i] * 2;
+        }
+        if(numArrLess1E[i] > 9) {
+            numArrLess1E[i] = numArrLess1E[i] - 9;
+        }
+        sum += numArrLess1E[i];
+    }
+    return (sum % 10 === parseInt(ccNum.charAt(ccNum.length - 1)));
+
+}
+
+function validatePostalCode(postalCode, countryCode) {
+    switch (countryCode) {
+        case "US":
+            postalCodeRegex = /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/;
+            break;
+        case "CA":
+            postalCodeRegex = /^([A-Z][0-9][A-Z])\s*([0-9][A-Z][0-9])$/;
+            break;
+        default:
+            postalCodeRegex = /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/;
+    }
+    return postalCodeRegex.test(postalCode);
+}
+
+function validateCVV(cvv) {
+    return (/^\d+$/.test(cvv) && cvv.length === 3);
+}
+
+
 $(document).ready(function() {
     /*
      *   Set focus on the first text field
@@ -14,6 +57,18 @@ $(document).ready(function() {
     $("#payment").val("credit card");
     $("#paypal").hide();
     $("#bitcoin").hide();
+
+    $("#nameErr").html("This field is required");
+    $("#nameErr").hide();
+    $("#emailErr").html("A valid email address is required");
+    $("#emailErr").hide();
+    var regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    $("#activityErr").html("At least one activity is required");
+    $("#activityErr").hide();
+    var next = 0;
+
+    $("#paymentErr").html("Payment option must be selected");
+    $("#paymentErr").hide();
 
 
 
@@ -108,14 +163,109 @@ $(document).ready(function() {
                 $("#paypal").show();
                 $("#bitcoin").hide();
                 break;
-            default:
+            case "bitcoin":
                 $("#credit-card").hide();
                 $("#paypal").hide();
                 $("#bitcoin").show();
                 break;
+            default:
+                $("#credit-card").hide();
+                $("#paypal").hide();
+                $("#bitcoin").hide();
+                break;
         }
     });
 
+    /*
+     *   Form validation
+     */
+     $("button[type=submit]").click(function(event) {
+        var name = $("#name").val();
+        if(!name) {
+            $("#nameErr").show();
+            $("#name").css("border-color", "red");
+        } else {
+            $("#name").css("border-color", "#c1deeb");
+            $("#nameErr").hide();
+            next++;
+        }
+        var email = $("#mail").val();
+        if(email == '' || !regex.test(email)) {
+            $("#emailErr").show();
+            $("#mail").css("border-color", "red");
+        } else {
+            $("#mail").css("border-color", "#c1deeb");
+            $("#emailErr").hide();
+            next++;
+        }
+        if(total < 100) {
+            $("#activityErr").show();
+        } else {
+            $("#activityErr").hide();
+            next++;
+        }
+        var payment = $("#payment").val();
+        if(payment === "select_method") {
+            $("#paymentErr").show();
+        } else {
+            $("#paymentErr").hide();
+            next++;
+        }
+
+        event.preventDefault();
+
+        if(payment === "credit card") {
+            var ccNum = $("#cc-num").val();
+            var isCC = validateCCNum(ccNum);
+            var postalCode = $("#zip").val();
+            var isPC = validatePostalCode(postalCode, "CA");
+            var cvv = $("#cvv").val();
+            var isCVV = validateCVV(cvv);
+            if(isCC && isPC && isCVV) {
+                next++;
+            } else {
+                var errMsgs = [];
+                if(!isCC) {
+                    var str = "Valid credit card number is required";
+                    $("#cc-num").css("border-color", "red");
+                    errMsgs.push(str);
+                } else {
+                    $("#cc-num").css("border-color", "#c1deeb");
+                }
+                if(!isPC) {
+                    var str = "Valid postal code is required";
+                    $("#zip").css("border-color", "red");
+                    errMsgs.push(str);
+                } else {
+                     $("#zip").css("border-color", "#c1deeb");
+                }
+                if(!isCVV) {
+                    var str = "Valid CVV is required"
+                    $("#cvv").css("border-color", "red");
+                    errMsgs.push(str);
+                } else {
+                    $("#cvv").css("border-color", "#c1deeb");
+                }
+                var errMsg = "";
+                for(var i = 0; i < errMsgs.length; i++) {
+                    errMsg = errMsg + (i+1) + ") " + errMsgs[i] + " ";
+                }
+                $("#ccErr").html(errMsg);
+            }
+
+
+        }
+
+        if(next === 5) {
+             $("form").submit(function() {
+                alert("Form validated.");
+            });
+        } else {
+            event.preventDefault();
+            next = 0;
+        }
+
+     });
 
 
 });
